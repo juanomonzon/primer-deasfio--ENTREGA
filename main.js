31ca9d1fbfd92247399992f50569e5dddeaea3bc
juanomonzon <juanomonzon@gmail.com>
2022-12-22 23:22:53 -0300

    segunda entrega

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const productos = [
     {nombre: "leche", precio: 150},
     {nombre: "galletitas" , precio: 100},
@@ -7,61 +6,63 @@ const productos = [
     {nombre: "azucar" , precio: 100},
     {nombre: "arroz" , precio: 150},
 ];
-let carrito = []
-let seleccion = prompt("Hola ¿desea comprar algo? si o no.")
 
-while(seleccion != "si" && seleccion != "no"){
-    alert("por favor ingrese si o no")
-    seleccion = prompt("Hola ¿desea comprar algo? si o no.")
+let carrito = [];
+
+// Obtiene el formulario de selección de producto y el formulario de cantidad de unidades
+const formularioProducto = document.querySelector("#formulario-producto");
+const formularioUnidades = document.querySelector("#formulario-unidades");
+
+// Obtiene el botón de agregar al carrito y escucha el evento click
+const botonAgregar = document.querySelector("#agregar-al-carrito");
+botonAgregar.addEventListener("click", () => {
+    // Obtiene el valor del formulario de selección de producto y el formulario de cantidad de unidades
+    const producto = formularioProducto.querySelector("#producto").value;
+    const unidades = formularioUnidades.querySelector("#unidades").value;
+
+    // Busca el producto en el listado de productos y obtiene su precio
+    const productoEncontrado = productos.find((p) => p.nombre === producto);
+    const precio = productoEncontrado ? productoEncontrado.precio : 0;
+
+    // Agrega el producto al carrito
+    carrito.push({ producto, unidades, precio });
+
+    // Muestra el carrito en pantalla
+    mostrarCarrito();
+});
+
+// Función para mostrar el carrito en pantalla
+function mostrarCarrito() {
+    const carritoDiv = document.querySelector("#carrito");
+    carritoDiv.innerHTML = "";
+
+    // Recorre el carrito y crea un elemento li por cada producto
+    carrito.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerText = `Producto: ${item.producto}, Unidades: ${item.unidades}, Total: ${item.precio * item.unidades}`;
+        carritoDiv.appendChild(li);
+    });
 }
 
-if(seleccion == "si"){
-    alert("a continuacion se mostrara nuestro listado de productos")
-    let todosLosProductos = productos.map((producto) => `${producto.nombre}  $${producto.precio}`);
-    alert(todosLosProductos.join(" - "))
-}else if(seleccion == "no"){
-    alert("gracias, vuelva prontos")
+// Carga el carrito del local storage al inicio
+const carritoStorage = localStorage.getItem("carrito");
+if (carritoStorage) {
+    carrito = JSON.parse(carritoStorage);
+    mostrarCarrito();
 }
-while(seleccion != "no"){
-    let producto = prompt("agrega un producto a tu carrito")
-    let precio = 0
-    
-    if(producto == "leche" || producto == "galletitas" || producto == "cerveza" || producto == "manteca" || producto == "azucar" || producto == "arroz"){
-        switch(producto){
-            case "leche":
-                precio = 150 ;
-                break;
-            case "galletitas":
-                precio = 100;
-                break;
-            case "cerveza":
-                precio = 230 ;
-                break;
-            case "manteca":
-                precio = 200 ;
-                break;
-            case "azucar":
-                precio = 100 ;
-                break;
-            case "arroz":
-                precio = 150;
-                break;
-        }
-        let unidades =parseInt(prompt("¿cuantas unidades quiere llevar?"))
-        carrito.push({producto, unidades, precio})
-        console.log(carrito)
-    }else{
-        alert("no tenemos ese producto")
-    }
 
-    seleccion = prompt("desea seguir comprando? si o no.")
-    while(seleccion === "no"){
-        alert("gracias, vuelva prontos")
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto : ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto: ${carritoFinal.unidades * carritoFinal.precio}`)
-        })
-        break
-    }
-}
- const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
- console.log(`el total a pagar es: ${total}`)
+// Al cerrar la página, guarda el carrito en el local storage
+window.addEventListener("beforeunload", () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+});
+
+// Botón para finalizar la compra y mostrar el total
+const botonFinalizar = document.querySelector("#finalizar-compra");
+botonFinalizar.addEventListener("click", () => {
+    // Calcula el total de la compra sumando los precios de cada producto por sus unidades
+    const total = carrito.reduce((acc, item) => acc + item.precio * item.unidades, 0);
+
+    // Muestra el total en pantalla
+    alert(`Total a pagar: ${total}`);
+});
+
